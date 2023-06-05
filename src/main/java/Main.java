094e37d4a86e0b601bea9d275f9cfef926a832c3
Diamond <fkvfptq1408@gmail.com>
2023-06-05 19:25:49 +0300

    HW2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
-import HW_1.Task1;
-import HW_1.Task2;
-import HW_1.Task3;
+import HW2.Task1;
+import HW2.Task2;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Task1 task1 = new Task1();
-        System.out.printf("%d\n", task1.getTriangleNumber(10));
+        String queryPart = "select * from students where ";
+        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        String sqlQuery = task1.generateSqlQuery(queryPart, json);
+        System.out.println(sqlQuery);
         Task2 task2 = new Task2();
-        System.out.println(task2.getFactorial(5));
-        Task3 calculator = new Task3();
-        calculator.calculate();
+        try {
+            String str = task2.readJsonFromFile("students.json");
+            task2.generateTextFromJson(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
