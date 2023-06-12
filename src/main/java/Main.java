1ecf22c317cf88b5dff64cd1747821db9b2abd16
Diamond <fkvfptq1408@gmail.com>
2023-06-12 20:04:48 +0300

    HW6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,20 +20,41 @@
 //    }
 //}
 
-import HW3.Task1;
-import java.util.ArrayList;
+
+
+import HW6.Notebook;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        HW3.Task1 allTasks = new HW3.Task1();
-        ArrayList<Integer> array = allTasks.generateIntList(10, 100);
-        System.out.println(array.toString());
-        System.out.printf("min: %d\n", allTasks.getMinNumber(array));
-        System.out.printf("max: %d\n", allTasks.getMaxNumber(array));
-        System.out.printf("average: %d\n", allTasks.getAverage(array));
-        System.out.printf("removed all even number:\n%s\n", allTasks.removeEvenNumbers(array));
+        Set<Notebook> notebooks = new HashSet<>();
+        notebooks.add(new Notebook("Macbook Pro", "Apple", 2000.0, "Silver", 13.3, 8, 256));
+        notebooks.add(new Notebook("ThinkPad X1 Carbon", "Lenovo", 1500.0, "Black", 14.0, 16, 512));
+        notebooks.add(new Notebook("Surface Laptop 3", "Microsoft", 1800.0, "Platinum", 13.5, 16, 512));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter filter criteria:");
+        System.out.print("Brand: ");
+        String brand = scanner.nextLine();
+        System.out.print("Max price: ");
+        double maxPrice = scanner.nextDouble();
+        System.out.print("Min screen size: ");
+        double minScreenSize = scanner.nextDouble();
+
+        filterNotebooks(notebooks, brand, maxPrice, minScreenSize);
+    }
+
+    public static void filterNotebooks(Set<Notebook> notebooks, String brand, double maxPrice, double minScreenSize) {
+        for (Notebook notebook : notebooks) {
+            if (notebook.getBrand().equals(brand) && notebook.getPrice() <= maxPrice && notebook.getScreenSize() >= minScreenSize) {
+                notebook.printNotebookInfo();
+            }
+        }
     }
 }
-
 
 
